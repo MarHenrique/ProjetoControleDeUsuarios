8927234a000e6a0db6ff7b2c9982d2f173f2d998
Lucas Lopes <lucasbadv8@gmail.com>
2016-03-30 19:18:48 -0300

    First !

---
+++ ProjetoTaFeito/ProjetoTaFeito.ControleDeUsuario/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetoTaFeito.ControleDeUsuario.MvcApplication" Language="C#" %>
---
+++ ProjetoTaFeito/ProjetoTaFeito.ControleDeUsuario/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoTaFeito.ControleDeUsuario.Startup))]
+namespace ProjetoTaFeito.ControleDeUsuario
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
